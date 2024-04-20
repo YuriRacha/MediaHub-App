3d5f83bb65c5d7810375f29f7c54f00befb402bf
YuriRacha <89609251+YuriRacha@users.noreply.github.com>
2024-04-19 22:51:07 -0400

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBFVEl2zsnDg0C-XFQh3XDj2g8BPy4hQnE",
+    authDomain: "mediahub-app.firebaseapp.com",
+    projectId: "mediahub-app",
+    storageBucket: "mediahub-app.appspot.com",
+    messagingSenderId: "1067574236614",
+    appId: "1:1067574236614:web:7322bbf93396033157cec7",
+    measurementId: "G-F03163EZXT"
+  };
+
+  export default firebaseConfig
